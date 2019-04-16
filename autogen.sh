f2c4428212ed9fcf03b7aa2a25c8bea5d43b1bce
qaz <fkxxyz@163.com>
2019-04-16 21:01:00 +0800

    add scripts

--- autogen.sh
+++ autogen.sh
@@ -4,12 +4,14 @@ name=ssr2json
 ver=1.0.0
 bug_report_address=sssssss
 
+extra_tool='ssr2jsonsave ssrsub ssrsubsave'
 
 [ "$1" == "clean" ] && \
 	rm -rf aclocal.m4 autom4te.cache compile configure install-sh \
 	autoscan.log config.h.in config.log configure.ac missing configure.scan \
 	Makefile.am config.status COPYING depcomp INSTALL Makefile.in Makefile \
-	stamp-h1 config.h .deps src/{Makefile.am,Makefile,Makefile.in,.deps} && \
+	stamp-h1 config.h .deps src/{Makefile.am,Makefile,Makefile.in,.deps} \
+	bin/{Makefile.am,Makefile,Makefile.in,.deps} && \
 	exit
 
 bin_PROGRAMS=${name}
@@ -17,12 +19,17 @@ eval ${bin_PROGRAMS}_SOURCES=\"$(cd src && echo *.cpp)\"
 
 echo "
 AUTOMAKE_OPTIONS = foreign
-SUBDIRS = src
+SUBDIRS = src bin
 " > Makefile.am
+
 echo "
 bin_PROGRAMS = ${bin_PROGRAMS}
 ${bin_PROGRAMS}_SOURCES = $(eval echo \${${bin_PROGRAMS}_SOURCES})
 " > src/Makefile.am
+
+echo "
+dist_bin_SCRIPTS = ${extra_tool}
+" > bin/Makefile.am
 
 autoscan
 sed -e "/AC_INIT(.*)/aAM_INIT_AUTOMAKE" \
