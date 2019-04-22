09d6f17915382059f55a36a6e16eb385f943abae
qaz <fkxxyz@163.com>
2019-04-23 02:02:20 +0800

    update

--- bin/ssrtest.py
+++ bin/ssrtest.py
@@ -10,14 +10,19 @@ import time
 import requests
 import json
 import threading
+import queue
 
-max_seconds = 10
+max_seconds = 4
 max_parallel_test = 16
 max_test_time = 4
 
 remain = 0
+remain_mutex = threading.Lock()
+devNull = open(os.devnull, 'w')
 
 event = threading.Event()
+
+queue_out = queue.Queue()
 
 
 def get_a_free_port():
@@ -29,7 +34,7 @@ def get_a_free_port():
 
 def test_a_node(node):
     port = get_a_free_port()
-    process = subprocess.Popen(['sslocal', "-l", str(port), "-c", node['file']])
+    process = subprocess.Popen(['sslocal', "-l", str(port), "-c", node['file']], stdout = devNull, stderr = devNull)
     proxies = {'http': 'socks5://127.0.0.1:' + str(port), 'https': 'socks5://127.0.0.1:' + str(port)}
     time.sleep(1)
     
@@ -41,35 +46,54 @@ def test_a_node(node):
             jr = requests.get("https://api-ipv4.ip.sb/geoip", proxies=proxies, timeout=max_seconds).text
             if json.loads(jr)['country_code'] != 'CN':
                 ok = True
-        except:
+        except :
             ok = False
-        if not ok:
-            continue
-        interval = time.time() - t_start
-        if interval < min_seconds:
-            min_seconds = interval
+        if ok:
+            interval = time.time() - t_start
+            if interval < min_seconds:
+                min_seconds = interval
+        else:
+            min_seconds = -1
+        
+        node["tested"] = t
+        node['delay_list'].append(str(min_seconds))
+        event.set()
     process.send_signal(signal.SIGINT)
-
-    node_info['result'] = -1 if min_seconds == max_seconds else min_seconds
-
-    remain -= 1
+    
+    node['result'] = min_seconds
+    
+    global remain, remain_mutex
+    if remain_mutex.acquire():
+        remain -= 1
+        remain_mutex.release()
+    queue_out.put(node['index'])
     event.set()
 
 def main():
     node_arr = []
+    index = 0
     for line in sys.stdin:
         new_node = dict()
+        new_node['index'] = index
         new_node['file'] = line.strip()
+        new_node['tested'] = 0
+        new_node['delay_list'] = []
         node_arr.append(new_node)
+        index += 1
     
     pool = threadpool.ThreadPool(max_parallel_test)
     reqs = threadpool.makeRequests(test_a_node, node_arr)
     [pool.putRequest(req) for req in reqs]
     
-    
+    global remain
     remain = len(node_arr)
     while remain > 0:
-        event.eait()
+        event.clear()
+        event.wait()
+        
+        while queue_out.qsize() > 0:
+            index = queue_out.get()
+            sys.stdout.write('\rNo.' + str(index) + '\t' + str(node_arr[index]['result']) + ' ' + ' '.join(node_arr[index]['delay_list']) + '\n')
 
 
 if __name__ == '__main__':
