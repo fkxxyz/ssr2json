7047872203f9a2390ce472a347494a8beaecb4d8
qaz <fkxxyz@163.com>
2019-06-15 20:46:41 +0800

    update

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ## 工具介绍
 ### ssr2json
-c语言编译出的二进制程序，将 ssr:// 开头的链接地址转换为 json 数据格式并输出到标准输出。
+c语言编译出的二进制程序，将 ssr:// 开头的链接地址转换为 json 数据格式并输出到标准输出，本地端口号均设为 1080。
 ### ssr2jsonsave
 shell 脚本，用于批量处理，从标准输入的每一行的 ssr:// 链接，均转换为 json 数据格式，并保存到指定目录中，每一组单独文件夹名，每一个名称对应文件名。
 ### ssrsub
@@ -16,26 +16,67 @@ shell 脚本，用于批量处理，从标准输入的每一行的订阅链接�
 shell 脚本，用于储存和管理订阅链接和节点，可以选择性的切换某个节点（需要python版的[shadowsocksr](https://github.com/shadowsocksr-backup/shadowsocksr)支持，并添加进systemd 服务设置）。
 
 ## 安装方法
+在 archlinux 中，可以搜索 aur 仓库里的 ssr2json 包可用，其它发行版可按照下列方法自行编译安装。
+
 克隆该仓库
+
 ```
 git clone https://github.com/fkxxyz/ssr2json.git
 ```
 进入仓库目录
+
 ```
 cd ssr2json
 ls
 ```
 自动生成 configure 脚本
+
 ```
 ./autogen.sh
 ```
 配置、编译、安装三连
+
 ```
 ./configure --prefix=/usr
 make
 make install
 ```
 
+## 使用方法
+用 ssrctl 命令可以对订阅地址和节点链接进行管理，两大模块，sub（订阅地址管理）模块和node（节点链接管理）模块，均能进行增删改查的操作。
 
+### 订阅地址管理
 
+```
+# 添加订阅地址
+ssrctl sub add <订阅地址>
+
+# 列出所有已添加的订阅地址
+ssrctl sub list
+
+# 删除一个订阅地址（地址号为列出时前面的标号）
+ssrctl sub del <订阅地址号>
+
+# 更新一个订阅地址
+ssrctl sub updateone <订阅地址号>
+
+# 更新所有订阅地址
+ssrctl sub update
+```
+
+### 节点链接管理
+
+```
+# 添加一个节点链接
+ssrctl node add <ssr链接>
+
+# 列出所有已添加的节点链接
+ssrctl node list
+
+# 删除一个节点链接（节点链接号为列出时前面的标号）
+ssrctl node del <节点链接号>
+
+# 连接一个节点（需要安装 python 版的 shadowsocksr 及其服务）
+ssrctl node connect <节点链接号>
+```
 
--- bin/ssrtest.py
+++ bin/ssrtest.py
@@ -12,9 +12,9 @@ import json
 import threading
 import queue
 
-max_seconds = 4
+max_seconds = 2
 max_parallel_test = 4
-max_test_time = 4
+max_test_time = 2
 
 remain = 0
 remain_mutex = threading.Lock()
@@ -27,7 +27,8 @@ queue_out = queue.Queue()
 def conv_delay(v):
     if v == 0:
         return 'x'
-
+    else:
+        return str(int(v*1000))
 
 def get_a_free_port():
     s = socket.socket()
@@ -45,26 +46,39 @@ def test_a_node(node):
     min_seconds = max_seconds
     for t in range(max_test_time):
         t_start = time.time()
+        retest = True
         ok = False
-        try:
-            jr = requests.get("https://api-ipv4.ip.sb/geoip", proxies=proxies, timeout=max_seconds).text
-            if json.loads(jr)['country_code'] != 'CN':
-                ok = True
-        except :
-            ok = False
+        while retest:
+            try:
+                jr = requests.get("https://api-ipv4.ip.sb/geoip", proxies=proxies, timeout=max_seconds).text
+                if json.loads(jr)['country_code'] != 'CN':
+                    ok = True
+                retest = False
+            except requests.exceptions.ConnectionError as e:
+                if (str(e).find('Max retries exceeded') != -1):
+                    retest = True
+                    time.sleep(0.5)
+                else:
+                    ok = False
+                    retest = False
+            except Exception as e:
+                ok = False
+                retest = False
         if ok:
             interval = time.time() - t_start
             if interval < min_seconds:
                 min_seconds = interval
         else:
-            min_seconds = 0
+            interval = 0
         
         node["tested"] = t
-        node['delay_list'].append(str(min_seconds))
+        node['delay_list'].append(conv_delay(interval))
         event.set()
     process.send_signal(signal.SIGINT)
     
-    node['result'] = min_seconds
+    if min_seconds == max_seconds:
+        min_seconds = 0
+    node['result'] = conv_delay(min_seconds)
     
     global remain, remain_mutex
     if remain_mutex.acquire():
@@ -97,7 +111,10 @@ def main():
         
         while queue_out.qsize() > 0:
             index = queue_out.get()
-            sys.stdout.write('\rNo.' + str(index) + '\t' + str(node_arr[index]['result']) + ' ' + ' '.join(node_arr[index]['delay_list']) + '\n')
+            sys.stdout.write('\r' + str(index + 1) + '\t' + str(node_arr[index]['result']) + ' ' + ' '.join(node_arr[index]['delay_list']) + '\n')
+        
+        #print('workRequests = ' + str(len(pool.workRequests)))
+        #print('workers = ' + str(pool.workers))
 
 
 if __name__ == '__main__':
